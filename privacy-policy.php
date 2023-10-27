02b22a521cd56c961e20f50680e690e16462b604
Ziasolkar <zia.prixim@gmail.com>
2023-10-27 12:58:26 +0530

    changes done in files

---
+++ privacy-policy.php
@@ -0,0 +1,19 @@
+<?php include("header.php"); ?>
+<section class="breadcrumb__area pt-100 pb-120 breadcrumb__overlay"
+   data-background="assets/img/banner/breadcrumb-01.jpg">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-xl-7 col-lg-12 col-md-12 col-12">
+            <div class="tp-breadcrumb">
+               <h2 class="tp-breadcrumb__title">Privacy Policy</h2>
+            </div>
+         </div>
+         <div class="col-xl-5 col-lg-12 col-md-12 col-12">
+            <div class="tp-breadcrumb__link text-xl-end">
+               <span>PRAVILABS : <a href="#">Privacy Policy</a></span>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<?php include("footer.php"); ?>
---
+++ terms-and-conditions.php
@@ -0,0 +1,101 @@
+<?php include("header.php"); ?>
+<section class="breadcrumb__area pt-100 pb-120 breadcrumb__overlay"
+   data-background="assets/img/banner/breadcrumb-01.jpg">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-xl-7 col-lg-12 col-md-12 col-12">
+            <div class="tp-breadcrumb">
+               <h2 class="tp-breadcrumb__title">Terms and Conditions</h2>
+            </div>
+         </div>
+         <div class="col-xl-5 col-lg-12 col-md-12 col-12">
+            <div class="tp-breadcrumb__link text-xl-end">
+               <span>PRAVILABS : <a href="#">Terms and Conditions</a></span>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="services-details pt-130 pb-120">
+            <div class="container">
+            
+               <div class="row">
+                  <div class="col-lg-12">
+                     <div class="tp-srv-process mb-50">
+                        <h4 class="tp-srv-process__title mb-30">Welcome to Pravilabs!
+                        </h4>
+                        <p class="mb-20">Must explain to you how all this mistaken idea of denouncing works pleasure and praising uts pain was born and I will gives you a itself completed account of the system, and sed expounds the ut actual teachings of that greater sed explores truth. Denouncing works pleasures and praising pains was us born and I will gives you a completed ut workers accounts of the system. sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                        <p class="mb-40">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem.</p>
+                        <div class="tp-srv-process__list">
+                           <ul>
+                              <li><i class="fa-solid fa-check"></i>Consectetur, adipisci velit, sed quia non numquam eius modi </li>
+                              <li><i class="fa-solid fa-check"></i>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquen</li>
+                              <li><i class="fa-solid fa-check"></i>Ut enim ad minima veniam, quis nostrum exercitationem</li>
+                              <li><i class="fa-solid fa-check"></i>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-lg-12">
+                     <div class="tp-srv-stap mb-40">
+                        <h4 class="tp-srv-stap__title mb-25">4 Simple Steps</h4>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+                        <p>Must explain to you how all this mistaken idea of denouncing works pleasure and praising uts pain was born and I will gives you a itself completed account of the system, and sed expounds the ut actual teachings of that greater sed explores truth. Denouncing works pleasures and praising pains was us born and I will gives you a completed ut workers accounts of the system. sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                        <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem.</p>
+                     </div>
+                  </div>
+                  <div class="col-xl-3 col-lg-4 col-md-6">
+                     <div class="tp-srv-stap__list mb-30 wow fadeInUp" data-wow-delay=".2s" style="visibility: hidden; animation-delay: 0.2s; animation-name: none;">
+                        <ul>
+                           <li><i class="fa-solid fa-check"></i>Extramural Funding</li>
+                           <li><i class="fa-solid fa-check"></i>Bacteria Markers</li>
+                           <li><i class="fa-solid fa-check"></i>Nam nec mi euismod euismod</li>
+                           <li><i class="fa-solid fa-check"></i>In aliquet dui nec lectus</li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-6">
+                     <div class="tp-srv-stap__list ml-65 mb-30 wow fadeInUp" data-wow-delay=".4s" style="visibility: hidden; animation-delay: 0.4s; animation-name: none;">
+                        <ul>
+                           <li><i class="fa-solid fa-check"></i>Extramural Funding</li>
+                           <li><i class="fa-solid fa-check"></i>Bacteria Markers</li>
+                           <li><i class="fa-solid fa-check"></i>Nam nec mi euismod euismod</li>
+                           <li><i class="fa-solid fa-check"></i>In aliquet dui nec lectus</li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-xl-5 col-lg-4 col-md-6">
+                     <div class="tp-srv-stap__list ml-65 ml-20 mb-30 wow fadeInUp" data-wow-delay=".6s" style="visibility: hidden; animation-delay: 0.6s; animation-name: none;">
+                        <ul>
+                           <li><i class="fa-solid fa-check"></i>Extramural Funding</li>
+                           <li><i class="fa-solid fa-check"></i>Bacteria Markers</li>
+                           <li><i class="fa-solid fa-check"></i>Nam nec mi euismod euismod</li>
+                           <li><i class="fa-solid fa-check"></i>In aliquet dui nec lectus</li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-lg-12">
+                     <div class="tp-srv-research mb-50">
+                        <h4 class="tp-srv-research__title mb-25">Our Research</h4>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                        </p>
+                     </div>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="tp-srv-bg mb-70">
+                        <img src="assets/img/services/services-thumb-09.jpg" alt="services-bg">
+                     </div>
+                     <div class="services-link tp-srv-link">  
+                        <span>Laboratories Used For Scientific Research :<a href="contact.html">OUr Project<i class="fa-solid fa-arrow-right"></i></a></span>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+<?php include("footer.php"); ?>
