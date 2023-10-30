946af0e5ddaee43a06738d4a36a6a77e58ad667b
jyoti12yyadav <jyoti.prixim@gmail.com>
2023-10-30 15:24:20 +0530

    copyright

---
+++ copyright.php
@@ -0,0 +1,67 @@
+<?php include("header.php"); ?>
+
+<section class="breadcrumb__area pt-100 pb-120 breadcrumb__overlay"
+    data-background="assets/img/banner/breadcrumb-01.jpg">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-xl-7 col-lg-12 col-md-12 col-12">
+                <div class="tp-breadcrumb">
+                    <h1 class="tp-breadcrumb__title">Copyright</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="services-details pt-130 pb-120">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="tp-srv-process mb-50">
+                    <h2 class="tp-srv-process__title mb-30">Copyright
+
+                    </h2>
+                    This website and its content, including but not limited to text, graphics, images, logos, and audio or video clips, are the property of Pravilabs.in and are protected by copyright laws. You may access, download, and print materials from this website for your personal and non-commercial use. However, any other use, including reproduction, modification, distribution, transmission, display, or performance of the content on this website is strictly prohibited without the express written consent of Pravilabs.in .
+
+                    </p>
+                    <p class="mb-40">The information provided on this website is intended for general informational purposes only and should not be considered as a substitute for professional medical advice, diagnosis, or treatment. The content on this website is provided "as is" and Pravi Labs makes no warranties, express or implied, regarding its accuracy, completeness, or fitness for a particular purpose.
+
+                    You agree not to use this website for any unlawful or prohibited activities and shall comply with all applicable laws and regulations. You are solely responsible for the use of this website, its content, and any materials you download or access from it.
+                    
+                    Any unauthorized use of the materials on this website may violate copyright, trademark, and other applicable laws and could result in criminal or civil penalties.
+                    
+                    Please carefully study the documents posted on this website: Legal information and Disclaimers, Privacy Policy, Cookie Policy and Website Summary. These documents are available only in English. You acknowledge that you know the English language at a sufficient level to understand the text of these documents.
+                    </p>
+
+
+                    <p class="mb-40">For inquiries regarding the use of our content or to request permission for any specific use, please contact:</p>
+                  
+                    <div class="row">
+                        <div class="col-3">
+                            
+                         
+                            <p class="mb-40">Pravi Labs .
+                                807, Realtech Park, Sector 30A, Vashi,
+                                Navi Mumbai - 400703, India
+                                Email: care@pravilabs.in
+                            </p>
+                           
+                        </div>
+                        <p class="mb-40">Pravi Labs reserves the right to change or modify this copyright notice at any time without prior notice. Please review this notice regularly to stay updated on our copyright policies.
+
+                        </p>
+                        <p class="mb-40">Thank you for visiting Pravilabs.in . Your compliance with these copyright terms is greatly appreciated.
+                        </p>
+                    </div>
+                  
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<?php include("footer.php"); ?>
